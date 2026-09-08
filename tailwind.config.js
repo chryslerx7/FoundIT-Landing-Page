5ec340bee047865be2b272b61c80b2f2e6e0daef
Jhed Sarmiento <chryslrosrmnt@gmail.com>
2026-09-08 11:45:24 +0800

    add real screenshots, make it pixelated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default { content: ['./index.html', './src/**/*.{js,jsx}'], theme: { extend: { fontFamily: { mono: ['"Space Mono"', 'monospace'], sans: ['Inter', 'sans-serif'], pixel: ['"Press Start 2P"', 'monospace'] } } }, plugins: [] }
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ['"Space Mono"', "monospace"],
+        sans: ["Inter", "sans-serif"],
+        pixel: ['"Press Start 2P"', "monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
