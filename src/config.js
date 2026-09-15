5306a795d20778facb8e28b72a39bb23a568c451
Jhed Sarmiento <chryslrosrmnt@gmail.com>
2026-09-15 14:29:28 +0800

    rename the apk, modified config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // Replace these two values when your release links are ready. Keep URLs centralized here.
-export const APK_DOWNLOAD_URL = "/FoundIT v1.0.1.apk";
+export const APK_DOWNLOAD_URL = "/FoundIT.apk";
 export const GITHUB_URL = "https://github.com/chryslerx7/FoundIT";
 export const APP_VERSION = "v1.0.1";
